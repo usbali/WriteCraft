b706b611d3ae1b14f93d36502964b60026aa02d1
Utkarsh Singh <117511135+usbali@users.noreply.github.com>
2023-12-07 12:17:15 +0530

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WriteCraft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
